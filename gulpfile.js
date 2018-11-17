1bbbcaff4822b9d23b03b97e2e9156f258fdf66e
CarlosVRL <carlos.varela.eng@gmail.com>
2018-11-17 02:19:14 -0600

    Install gulp with live browser reload task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,22 @@
-var gulp = require('gulp');
-gulp.task('default', function () {
-    console.log('Hello Gulp!')
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('html', function() {
+  console.log('reloading (not really)');
+  return gulp.src('Responsive*/**/*.html')
+            .pipe(browserSync.stream());
 });
+
+gulp.task('watch', function() {
+  var server = livereload();
+  gulp.watch('**/*.html', ['html']);
+});
+
+gulp.task('serve-survey-form', ['watch'], function() {
+    browserSync.init({
+        server: './Responsive Web Design Certification/Responsive Web Design Projects/Survey Form'
+    });
+});
+
+gulp.task('default', ['serve', 'watch']);
